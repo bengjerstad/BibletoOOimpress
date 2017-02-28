06ecfbfa265cb155a28299db5ceb45284d045a02
bengjerstad <bengjerstad@gmail.com>
2017-02-27 23:12:44 -0500

    Extract data from the databse file
    code to get the data out of the databse file one verse at a time.

---
+++ fromdb.py
@@ -0,0 +1,25 @@
+﻿# -*- coding: UTF-8 -*-
+import sys
+import json
+import sqlite3
+import time
+import pyperclip
+
+conn = sqlite3.connect('esv.db')
+c = conn.cursor()
+
+def getaverse(book,chapter,verse):
+	x = c.execute("SELECT * FROM "+book+" WHERE chapter = ? AND verse = ?",(chapter,verse))
+	for y in x:
+		text = y[2]
+		return verse,text
+
+
+
+v,text = getaverse('Genesis','1','5')
+
+#print("{}\u00b2".format())
+SUP = str.maketrans("0123456789", u"⁰¹²³⁴⁵⁶⁷⁸⁹")
+v=v.translate(SUP)
+pyperclip.copy(v+text)
+#spam = pyperclip.paste()
