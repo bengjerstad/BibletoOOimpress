7edfa55637d3ec9d67c8100c8bb3c3c703e04df1
bengjerstad <bengjerstad@gmail.com>
2017-02-28 13:52:44 -0500

    added getmultiverses function and write to file
    added getmultiverses function - a framework function for handeling
    input.
    Also, added write out to file.

--- fromdb.py
+++ fromdb.py
@@ -9,17 +9,30 @@ conn = sqlite3.connect('esv.db')
 c = conn.cursor()
 
 def getaverse(book,chapter,verse):
+	SUP = str.maketrans("0123456789", u"⁰¹²³⁴⁵⁶⁷⁸⁹")
+	v=verse.translate(SUP)
 	x = c.execute("SELECT * FROM "+book+" WHERE chapter = ? AND verse = ?",(chapter,verse))
 	for y in x:
 		text = y[2]
-		return verse,text
+		return v+text
+
+def getmultiverses(input):
+	fintext = ''
+	#parse the input
+	
+	#loop for each verse. 
+	atext = getaverse('Genesis','1','5')
+	#combine the txt for loops.
+	fintext = atext
+	
+	return fintext
 
 
+text = getmultiverses('Genesis 1:1-5')
 
-v,text = getaverse('Genesis','1','5')
-
-#print("{}\u00b2".format())
-SUP = str.maketrans("0123456789", u"⁰¹²³⁴⁵⁶⁷⁸⁹")
-v=v.translate(SUP)
-pyperclip.copy(v+text)
+#pyperclip.copy(text)
 #spam = pyperclip.paste()
+
+file = open('out.txt', 'w',encoding="UTF-8")
+file.write(text)
+file.close()
